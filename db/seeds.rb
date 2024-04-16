9992bd0ca9fecd9bf703c077fbf0d78e93390112
unknown <leonardorodriguesgomes@hotmail.com>
2024-04-15 22:09:24 -0300

    Adiciona faker no seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Hero.delete_all
+
+5.times do
+    Hero.create name: Faker::Superhero.name
+end
